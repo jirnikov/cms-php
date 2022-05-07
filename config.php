d42d10e828556e33c667675605b50eb7c78d530e
Jirnikov <jirnikov@rambler.ru>
2022-05-07 21:25:05 +0300

    Структура приложения

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('VG_ACCESS') or die('Нет доступа');
+
+const SITE_URL = 'http://cms-php.ru';
+const PATH = '/';
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'cmsphp';
+
+?>
